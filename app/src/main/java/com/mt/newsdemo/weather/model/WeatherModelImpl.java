3d794728b05360a6a0cf9f91461da8d7432824b2
unknown <meitao@mei110.corp.tigerjoys.com>
2016-12-21 18:19:15 +0800

    添加天气模块的网络逻辑

--- app/src/main/java/com/mt/newsdemo/weather/model/WeatherModelImpl.java
+++ app/src/main/java/com/mt/newsdemo/weather/model/WeatherModelImpl.java
@@ -1,22 +1,52 @@
 package com.mt.newsdemo.weather.model;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
+import android.text.TextUtils;
 
 import com.mt.newsdemo.beans.WeatherBean;
+import com.mt.newsdemo.commons.Urls;
+import com.mt.newsdemo.utils.LogUtil;
 import com.mt.newsdemo.utils.OkHttpUtil;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
  * Created by meitao on 2016/12/17.
  */
 
-public class WeatherModelImpl implements WeatherModel{
-
+public class WeatherModelImpl implements WeatherModel {
+    private static final String TAG = "WeatherModelImpl";
 
     @Override
-    public void loadWeatherData(String cityName, LoadWeatherLintener listener) {
+    public void loadWeatherData(String cityName, final LoadWeatherLintener listener) {
+            try{
+                String url = Urls.WEATHER+ URLEncoder.encode(cityName,"UTF-8");
+                OkHttpUtil.get(url, new OkHttpUtil.ResultCallBack<String>() {
+                    @Override
+                    public void onSuccess(String response) {
+                        // 解析json
+                        LogUtil.d(TAG,"11111111111"+response);
+                        // 数据回调
+                        listener.onSuccess(null);
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        LogUtil.d(TAG,"222222222"+e);
+                    }
+                });
+            }
+            catch (UnsupportedEncodingException e){
+                e.printStackTrace();
+            }
+
 
 
     }
@@ -24,15 +54,33 @@ public class WeatherModelImpl implements WeatherModel{
     @Override
     public void loadLocation(Context context, final LoadLocationLintener listener) {
         // 获取当前用户的经纬度
-        LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                listener.onFailure("location fail", null);
+                return;
+            }
 
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (location == null) {
+            listener.onFailure("location fail", null);
+            return;
+        }
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        String locationUrl = getLocationURL(latitude, longitude);
         // 将定位到的经纬度 通过网络请求来 当前用户所在城市的名称
-        OkHttpUtil.get("", new OkHttpUtil.ResultCallBack<String>() {
+        OkHttpUtil.get(locationUrl, new OkHttpUtil.ResultCallBack<String>() {
             @Override
             public void onSuccess(String response) {
                 // 将定位到城市的名称回掉到 presenter 中的 onsuccess ()
-                if(listener != null)
+                if (listener != null)
                     // 解析response
+                LogUtil.e(TAG, response);
 
                     listener.onSuccess("");
             }
@@ -46,13 +94,23 @@ public class WeatherModelImpl implements WeatherModel{
 
     }
 
-    public interface LoadWeatherLintener{
+    public String getLocationURL(double latitude, double longitude) {
+        StringBuffer sb = new StringBuffer(Urls.INTERFACE_LOCATION);
+        sb.append("?output=json").append("&referer=32D45CBEEC107315C553AD1131915D366EEF79B4");
+        sb.append("&location=").append(latitude).append(",").append(longitude);
+        LogUtil.d(TAG, sb.toString());
+        return sb.toString();
+    }
+
+    public interface LoadWeatherLintener {
         void onSuccess(List<WeatherBean> list);
+
         void onFailure(String msg, Exception e);
     }
 
-    public interface LoadLocationLintener{
+    public interface LoadLocationLintener {
         void onSuccess(String cityName);
+
         void onFailure(String msg, Exception e);
     }
 
--- app/src/main/java/com/mt/newsdemo/weather/presenter/WeatherPresenterImpl.java
+++ app/src/main/java/com/mt/newsdemo/weather/presenter/WeatherPresenterImpl.java
@@ -3,16 +3,20 @@ package com.mt.newsdemo.weather.presenter;
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.mt.newsdemo.beans.WeatherBean;
+import com.mt.newsdemo.utils.LogUtil;
 import com.mt.newsdemo.utils.ToolsUtil;
 import com.mt.newsdemo.weather.model.WeatherModel;
 import com.mt.newsdemo.weather.model.WeatherModelImpl;
 import com.mt.newsdemo.weather.view.WeatherView;
 
+import java.util.List;
+
 /**
  * Created by meitao on 2016/12/17.
  */
 
-public class WeatherPresenterImpl implements WeatherPresenter {
+public class WeatherPresenterImpl implements WeatherPresenter,WeatherModelImpl.LoadWeatherLintener{
     private WeatherModel mWeatherModel;
     private WeatherView mWeatherView;
     private Context mContext;
@@ -24,23 +28,37 @@ public class WeatherPresenterImpl implements WeatherPresenter {
         mWeatherView = weatherView;
     }
 
+    WeatherModelImpl.LoadLocationLintener linstener = new WeatherModelImpl.LoadLocationLintener() {
+        @Override
+        public void onSuccess(String cityName) {
+            LogUtil.d("WeatherPresenterImpl","获取成功");
+            mWeatherModel.loadWeatherData("" ,WeatherPresenterImpl.this);
+        }
+
+        @Override
+        public void onFailure(String msg, Exception e) {
+
+        }
+    };
     @Override
     public void loadWeatherData() {
         if (ToolsUtil.isNetworkAvailable(mContext)) {
-            mWeatherModel.loadLocation(mContext, new WeatherModelImpl.LoadLocationLintener() {
-                @Override
-                public void onSuccess(String cityName) {
-                    if(!TextUtils.isEmpty(cityName)){
-                        mWeatherView.setCity(cityName);
-                    }
-                }
+            mWeatherModel.loadLocation(mContext, linstener);
 
-                @Override
-                public void onFailure(String msg, Exception e) {
 
-                }
-            });
+
         }
+
+    }
+
+
+    @Override
+    public void onSuccess(List<WeatherBean> list) {
+
+    }
+
+    @Override
+    public void onFailure(String msg, Exception e) {
 
     }
 }
